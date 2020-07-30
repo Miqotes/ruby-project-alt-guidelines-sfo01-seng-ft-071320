9c7e91aa69a240b661c22dff5f1477cde3258fc7
Miqotes <ashleelennab@gmail.com>
2020-07-30 15:23:39 -0700

    MVP reached

--- bin/run.rb
+++ bin/run.rb
@@ -3,4 +3,10 @@ ActiveRecord::Base.logger.level = 1
 cli = CommandLineInterface.new
 cli.greet
 
-cli.enter_store_name
+#cli.enter_store_name
+
+#cli.create_review
+
+#cli.update_review
+
+cli.delete_review
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -39,6 +39,41 @@ class CommandLineInterface
     
     #lc_review = CatFoodReview.create(
     #review: Faker::Coffee.unique.notes
-    entered_review = CatFoodReview.create(review: review_text)
+    entered_review = CatFoodReview.create(review: review_text, rating: rating_num, brand_name: brand_name)
+    cat_user = Cat.find_or_create_by(name: cat_name)
+    entered_review.cat = cat_user
+    store = PetFoodStore.find_or_create_by(name: store_name)
+    entered_review.pet_food_store = store
+    entered_review.save
+    # CatFoodReview.create({ :review => review_text })
   end
+
+  def update_review
+    puts 'Enter user cat name:'
+    cat_name = gets.chomp
+    cat_user = Cat.find_by(name: cat_name)
+    reviews = cat_user.cat_food_reviews
+    puts 'Enter review number to change:'
+    update_review = gets.chomp.to_i
+    review = reviews[update_review - 1]
+    puts 'Enter your new review:'
+    review_text = gets.chomp
+    review.review = review_text
+    review.save
+    # collect review number from user
+    # edit review.review at number
+    # save changes to database
+  end
+  
+  def delete_review
+    puts 'Enter user cat name:'
+    cat_name = gets.chomp
+    cat_user = Cat.find_by(name: cat_name)
+    reviews = cat_user.cat_food_reviews
+    puts 'Enter review number to delete:'
+    delete_review = gets.chomp.to_i
+    review = reviews[delete_review - 1]
+    review.destroy
+  end
+  
 end
